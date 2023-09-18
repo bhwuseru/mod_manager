8bd9926215d164184bdeccd3a82c612cc5023292
Takurou Nakada <takurou403@gmail.com>
2023-09-18 09:24:33 +0900

    ディレクトリサービスにコメント追加

--- app/Services/DirectoryService.php
+++ app/Services/DirectoryService.php
@@ -5,33 +5,47 @@ namespace App\Services;
 class DirectoryService
 {
 
+    /**
+     * 指定されたディレクトリ内のディレクトリ一覧を取得します。
+     *
+     * @param string $path ディレクトリのパス
+     * @return array ディレクトリ名の配列
+     */
     public static function getDirs(string $path): array
     {
-
         $dirs = [];
+        // ディレクトリ内のファイルおよびディレクトリの一覧を取得
         $directories =  array_diff(scandir($path), ['.', '..']);
         foreach ($directories as $directory) {
-
-            $directoryPath = $path. DIRECTORY_SEPARATOR . $directory;
+            $directoryPath = $path . DIRECTORY_SEPARATOR . $directory;
+            // ディレクトリでない場合はスキップ
             if (!is_dir($directoryPath)) continue;
+            // ディレクトリ名を配列に追加
             array_push($dirs, $directory);
         }
-
         return $dirs;
     }
 
-    public static function getFiles(string $path, array $includeExtentsions  = []): array
+    /**
+     * 指定されたディレクトリ内のファイル一覧を取得します。
+     *
+     * @param string $path ディレクトリのパス
+     * @param array $includeExtensions ファイルの拡張子の配列
+     * @return array ファイル名またはファイル名（拡張子なし）の配列
+     */
+    public static function getFiles(string $path, array $includeExtensions = []): array
     {
-        if(!is_dir($path)) return [];
+        if (!is_dir($path)) return [];
 
         $directories =  array_diff(scandir($path), ['.', '..']);
         $files = [];
         foreach ($directories as $directory) {
-
             $directoryPath = $path . DIRECTORY_SEPARATOR . $directory;
+            // ディレクトリの場合はスキップ
             if (is_dir($directoryPath)) continue;
 
-            if (0 === count($includeExtentsions)) {
+            if (0 === count($includeExtensions)) {
+                // 拡張子のフィルタが設定されていない場合、ファイル名を配列に追加
                 array_push($files, $directory);
                 continue;
             }
@@ -39,11 +53,10 @@ class DirectoryService
             $fileExtension = pathinfo($directory, PATHINFO_EXTENSION);
             $fileNameWithExtension = pathinfo($directory, PATHINFO_FILENAME);
 
-            if (!in_array($fileExtension, $includeExtentsions)) continue;
+            if (!in_array($fileExtension, $includeExtensions)) continue;
+            // ファイル名（拡張子なし）を配列に追加
             array_push($files, $fileNameWithExtension);
         }
-
         return $files;
     }
-
 }
